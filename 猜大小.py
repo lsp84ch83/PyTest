d466a2cee2eb0b8f61eef5e8a9dade395fb491d5
lsp84ch83 <357003280@qq.com>
2017-08-16 17:29:25 +0800

    猜大小
    有次数限制

---
+++ 猜大小.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+import random
+
+guess = int(input('输入心中的数字：'))
+secret = random.randint(1,100)
+count = 3
+
+while count:
+    if guess == secret:
+        print('恭喜猜对了！')
+        break
+    else:
+        if guess > secret:
+            print('你猜大了')
+        else:
+            print('你猜小了')
+        count -= 1
+        print('你还有%d次机会,请继续：' %count,end='')
+        guess = int(input())
+
+    if count == 1:
+        break
+
+if count == 1:
+    print('游戏结束')
+    print('随机数字是%d' %secret)
