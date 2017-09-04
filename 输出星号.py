34f0ba42f2f4c037249919c6ef7b34f1f2180621
lsp84ch83 <357003280@qq.com>
2017-09-04 22:54:18 +0800

    默认的

---
+++ 输出星号.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+for i in range(1,4):
+    for f in range(0,i*2-1):
+        print('*',end='')
+    print()
+    
+for a in range(2,0,-1):
+    for s in range(0,a*2-1):
+        print('*',end = '')
+    print()
