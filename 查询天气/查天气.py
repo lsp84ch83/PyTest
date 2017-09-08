a0a6117a1ca7ad8dd25281ee2c882956fc4d860d
lsp84ch83 <357003280@qq.com>
2017-09-08 15:58:50 +0800

    默认的

--- 查询天气/查天气.py
+++ 查询天气/查天气.py
@@ -4,3 +4,17 @@ import urllib.request
 import json
 from city import city
 
+cityname = input('你想查询哪个城市的天气：')
+citycode = city.get(cityname)
+if citycode:
+    try:
+        url = ('http://www.weather.com.cn/data/cityinfo/%s.html' % citycode)
+        content = urllib.request.urlopen(url).read()
+        data = json.loads(content)
+        result = data['weatherinfo']
+        str_temp = ('%s\n%s - %s') % (result['weather'],result['temp1'],result['temp2'])
+        print(str_temp)
+    except:
+        print('查询失败')
+else:
+    print('没有找到该城市')
