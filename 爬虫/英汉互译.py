584c7e579e293c90d573ba981c073b02fe41963d
lsp84ch83 <357003280@qq.com>
2017-09-25 21:58:27 +0800

    默认的

--- 爬虫/英汉互译.py
+++ 爬虫/英汉互译.py
@@ -6,14 +6,17 @@ import json
 
 content = input('请输入需要翻译的内容：')
 url = 'http://fanyi.baidu.com/v2transapi'
+
 data = {}
 data['from'] = 'auto'
 data['to'] = 'auto'
 data['query'] = content
 data['transtype'] = 'translang'
 data['simple_means_flag'] = '3'
-
 data = urllib.parse.urlencode(data).encode('utf-8')
+
+req = urllib.request.Request(url,data)
+req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0')
 
 response = urllib.request.urlopen(url,data)
 html = response.read().decode('utf-8')
