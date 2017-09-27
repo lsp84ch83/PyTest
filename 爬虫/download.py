ed10103f8fcfe4ef6301cd7665ca45638488a0a2
lsp84ch83 <357003280@qq.com>
2017-09-27 16:20:25 +0800

    默认的

--- 爬虫/download.py
+++ 爬虫/download.py
@@ -6,7 +6,6 @@ def url_open(url):
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36')
     response = urllib.request.urlopen(url)
     html = response.read()
-
     return html
 
 def get_page(url):
