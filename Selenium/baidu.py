2d938374df43f5716888af9fafeb6889e93f40b4
lsp84ch83 <357003280@qq.com>
2017-10-24 19:11:24 +0800

    默认的

---
+++ Selenium/baidu.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.get('http://www.baidu.com')
+
+driver.find_element_by_class_name('s_ipt').send_keys('Python')
+driver.find_element_by_id('su').click()
+
+sleep(2)
+driver.quit()
