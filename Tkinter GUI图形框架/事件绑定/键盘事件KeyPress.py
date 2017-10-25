3d7a3ca9d048dd793b25e6c0a97fe971650e3382
lsp84ch83 <357003280@qq.com>
2017-10-25 23:29:48 +0800

    默认

---
+++ Tkinter GUI图形框架/事件绑定/键盘事件KeyPress.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+from tkinter import *
+
+root = Tk()
+
+def callback(event):
+    print(event.char)
+    # char 获取当前的字符
+
+frame = Frame(root, width=200, height=200)
+frame.bind("<Key>", callback)
+frame.focus_set()
+# 获得焦点
+frame.pack()
+
+mainloop()
