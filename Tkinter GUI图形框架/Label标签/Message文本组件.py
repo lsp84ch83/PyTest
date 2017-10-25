aca7cc0aa9962c36fb96e7116000d79e41d26ddd
lsp84ch83 <357003280@qq.com>
2017-10-25 23:29:15 +0800

    默认

---
+++ Tkinter GUI图形框架/Label标签/Message文本组件.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+'''
+Message 是Label组件的变体，用于显示多行文本消息；
+Message组件能够自动换行，并调整文本的尺寸使其适应给定的尺寸
+'''
+from tkinter import *
+
+root = Tk()
+
+w1 = Message(root, text="这是一则消息", width=100)
+w1.pack()
+
+w2 = Message(root, text="这是一则骇人听闻的非常难得一见的很长的消息", width=100)
+w2.pack()
+
+
+mainloop()
