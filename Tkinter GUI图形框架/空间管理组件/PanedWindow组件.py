c1f77fbef0fac228ce79d7fe392a9e65647fe1f5
lsp84ch83 <357003280@qq.com>
2017-10-26 21:51:16 +0800

    默认

---
+++ Tkinter GUI图形框架/空间管理组件/PanedWindow组件.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+'''
+PanedWindow组件是一个空间管理组件，跟Frame组件类似，
+都是为组件提供一个框架，不过PanedWindow允许让用户调整应用城的空间划分
+'''
+from tkinter import *
+
+m = PanedWindow(orient=VERTICAL)
+m.pack(fill=BOTH, expand=1)
+
+top = Label(m, text="top pane")
+m.add(top)
+
+bottom = Label(m, text="bottom pane")
+m.add(bottom)
+
+mainloop()
