3a0b4b79320282f96ec9d2217d6bf8bd2ab3ab98
lsp84ch83 <357003280@qq.com>
2017-10-31 22:41:08 +0800

    默认

--- Tkinter GUI图形框架/布局管理器/Grid按行-列形式排列组件.py
+++ Tkinter GUI图形框架/布局管理器/Grid按行-列形式排列组件.py
@@ -16,6 +16,9 @@ root = Tk()
 Label(root, text="用户名").grid(row=0, sticky = W)
 Label(root, text="密码").grid(row=1, sticky = W)
 
+photo = PhotoImage(file="8.gif")
+Label(root, image=photo).grid(row=0, column=2, rowspan=2, padx=5, pady=5)
+
 Entry(root).grid(row=0, column=1)
 Entry(root, show="*").grid(row=1, column=1)
 
