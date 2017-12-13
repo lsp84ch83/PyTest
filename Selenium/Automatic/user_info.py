a5f0b761dd24ab334f640cc4d84cc96f5dd01fe8
lsp84ch83 <357003280@qq.com>
2017-12-13 17:00:01 +0800

    默认

--- Selenium/Automatic/user_info.py
+++ Selenium/Automatic/user_info.py
@@ -30,3 +30,37 @@ for user in date:
     print(user[1] + "\n")  # 获取邮箱信息
 # -----------------------------------------------------------------------------------------
 '''
+# --------------------------使用读取 xml 来获取账号密码，解决不规则字段读取-------------------------
+from xml.dom import minidom
+
+dom = minidom.parse("info.xml")
+# # 得到 XML 文件的唯一根元素
+root = dom.documentElement
+# print(root.nodeName)    # 输出节点名称
+# print(root.nodeValue)   # 输出节点值
+# print(root.nodeType)    # 输出节点类型
+# print(root.ELEMENT_NODE)
+# ------------1.获得任意标签名 getElementByTagName() 可以通过标签名获取标签------------
+# tagname = root.getElementsByTagName('browser')
+# print(tagname[0].tagName)
+# tagname = root.getElementsByTagName("login")
+# print(tagname[1].tagName)
+# tagname = root.getElementsByTagName("province")
+# print(tagname[2].tagName)
+
+# ------------2.获得标签的属性值 getAttribute() 方法用千获取元素的属性值------------
+# logins = root.getElementsByTagName("login")
+# # 获得 logins 标签的 username 属性值
+# username = logins[0].getAttribute("username")
+# print(username)
+# # 获得 logins 标的 password 属性值
+# password = logins[0].getAttribute("password")
+# print(password)
+
+# ------------3.获得标签对之间的数据 firstChild 属性返回被选节点的第一个子节点; data 表示获取该节点的数据------------
+provinces = dom.getElementsByTagName("province")
+citys = dom.getElementsByTagName("city")
+p2 = provinces[1].firstChild.data   # 获得第二个 province 标签对的值
+print(p2)
+c1 = citys[0].firstChild.data # 获得第一个 citys 标签对的值
+print(c1)
