81f294b90c3d9b5f0b1853f7f12daa1850fab25b
lsp84ch83 <357003280@qq.com>
2017-12-14 17:33:26 +0800

    默认

---
+++ Selenium/unittest/calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/14 16:30
+# @Author  : Soner
+# @version : 
+# @license : Copyright(C), Your Company
+class Count:
+    def __init__(self, a, b):
+        self.a = int(a)
+        self.b = int(b)
+
+    # 计算加法
+    def add(self):
+        return self.a + self.b
---
+++ Selenium/unittest/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/14 16:32
+# @Author  : Soner
+# @version : 
+# @license : Copyright(C), Your Company
+from Selenium.unittest.calculator import Count
+import unittest
+
+# 测试两个整数相加
+class TestCount(unittest.TestCase):
+
+    def setUp(self):
+        print("=====初始化=====")
+
+    def test_add(self):
+        j = Count(2, 3)
+        self.assertEqual(j.add(), 5)    # 判断 j.add 的值是否等于5；assertTqual() 由 unittest 框架提供
+
+    def test_add2(self):
+        j = Count(212, 321)
+        self.assertEqual(j.add(), 12312)
+
+    def tearDown(self):
+        print("=====清理环境=====")
+
+
+if __name__ == '__main__':
+    # 构造测试集
+    suite = unittest.TestSuite()
+    suite.addTest(TestCount("test_add2"))
+    # 执行测试
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+'''
+首先，调用 unittest 框架的 TestSuite() 类来创建测试套件，
+通过它所提供的 addTest()方法来添加测试用例 test_add2(),
+接着调用 unittest 框架的 TextTestRunner()类，通过它下面run()方法来运行 suite 所组装的测试用例
+'''
