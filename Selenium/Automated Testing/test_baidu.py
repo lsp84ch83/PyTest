6c4076fc56ca23c1d940479a49f5da2e02e2a12e
lsp84ch83 <357003280@qq.com>
2018-01-10 18:21:38 +0800

    默认

---
+++ Selenium/Automated Testing/test_baidu.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/29 11:07
+# @Author  : Soner
+# @version : 
+# @license : Copyright(C), Your Company
+from selenium import webdriver
+import unittest
+from HTMLTestRunner import HTMLTestRunner
+from time import sleep
+
+class Baidu(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+        self.baidu_url = "http://www.baidu.com"
+
+    def test_baidu_search(self):
+        driver = self.driver
+        driver.get(self.baidu_url)
+        driver.find_element_by_id("kw").send_keys("sorce")
+        sleep(2)
+        driver.find_element_by_id("su").click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    testunit = unittest.TestSuite()
+    testunit.addTest(Baidu("test_baidu_search"))
+
+    # 定义报告存放的位置
+    fp = open("F:/PyTest/Selenium/Automated Testing/result.html", "wb")
+    # 定义测试报告
+    runner = HTMLTestRunner(stream=fp,
+                            title="百度搜索测试报告",
+                            description="用例执行情况：")
+    runner.run(testunit)
+    fp.close()
