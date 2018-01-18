1c0ca2a8a856846c6246109b6b50a79ca43726b4
lsp84ch83 <357003280@qq.com>
2018-01-19 01:26:59 +0800

    默认

--- 内部复件.py
+++ 内部复件.py
@@ -15,24 +15,14 @@ def _exec(cmd):
     proc = subprocess.Popen(cmd)
     proc.wait()
 
-
 def initADB():
-    '''
-    :启动adb
-    '''
+    #启动adb
     cmd = 'adb start-server'
     _exec(cmd)
-
 
 def initAPP(packagename):
     cmd = 'adb shell pm clear %s' % packagename
     _exec(cmd)
-
-
-def startAPP(luanchActivity):
-    cmd = 'adb shell am start %s' % luanchActivity
-    _exec(cmd)
-
 
 def getUserId(packagename):
     '''
@@ -67,8 +57,9 @@ def getFlowFromUid(packagename, uid=None):
     lo_snd_front = []
 
     for line in std:
-        data = line.split()
+
         if 'wlan0' in line:
+            data = line.split()
             background_flow = int(data[4]) == 0
             if background_flow:
                 net_rcv_bck.append(int(data[5]))
@@ -114,7 +105,7 @@ if __name__ == '__main__':
     # 初始化adb
     initADB()
     # 清除应用数据
-    initAPP(packagename)
+    #initAPP(packagename)
 
     # pid = getPID(packagename)
     uid = (getUserId(packagename))[0:5]
@@ -129,21 +120,18 @@ if __name__ == '__main__':
     lo_start_rx = lo_bck_start_rx + lo_front_start_rx
     lo_start_tx = lo_bck_start_tx + lo_front_start_tx
 
-    # 启动应用
-    # startAPP(launchActivity)
-
     col = 0
     row = 0
 
     book_Mirror = xlwt.Workbook(encoding='utf-8', style_compression=0)  # 创建新的工作簿Mirror
-    # book_Mirror_Server = xlwt.Workbook(encoding='utf-8', style_compression=0)  # 创建新的工作簿Mirror_Server
-    # book_TXZ = xlwt.Workbook(encoding='utf-8', style_compression=0)  # 创建新的工作簿TXZ
+    book_Mirror_Server = xlwt.Workbook(encoding='utf-8', style_compression=0)  # 创建新的工作簿Mirror_Server
+    book_TXZ = xlwt.Workbook(encoding='utf-8', style_compression=0)  # 创建新的工作簿TXZ
 
     sheet_load_Mirror = book_Mirror.add_sheet('流量', cell_overwrite_ok=True)  # 创建新的sheet，并命名为LOAD
 
-    # sheet_load_Mirror_Server = book_Mirror_Server.add_sheet('流量', cell_overwrite_ok=True)  # 创建新的sheet，并命名为LOAD
-    #
-    # sheet_load_TXZ = book_TXZ.add_sheet('流量', cell_overwrite_ok=True)  # 创建新的sheet，并命名为LOAD
+    sheet_load_Mirror_Server = book_Mirror_Server.add_sheet('流量', cell_overwrite_ok=True)  # 创建新的sheet，并命名为LOAD
+
+    sheet_load_TXZ = book_TXZ.add_sheet('流量', cell_overwrite_ok=True)  # 创建新的sheet，并命名为LOAD
 
     sheet_load_Mirror.write(row, col, "时间")
     sheet_load_Mirror.write(row, col + 1, "网络下行(KB)")
@@ -152,35 +140,36 @@ if __name__ == '__main__':
     sheet_load_Mirror.write(row, col + 4, "本地下行(KB)")
     sheet_load_Mirror.write(row, col + 5, "本地上行(KB)")
     sheet_load_Mirror.write(row, col + 6, "本地总流量(KB)")
-    #
-    # sheet_load_Mirror_Server.write(row, col, "时间")
-    # sheet_load_Mirror_Server.write(row, col + 1, "网络下行(KB)")
-    # sheet_load_Mirror_Server.write(row, col + 2, "网络上行(KB)")
-    # sheet_load_Mirror_Server.write(row, col + 3, "网络总流量(KB)")
-    # sheet_load_Mirror_Server.write(row, col + 4, "本地下行(KB)")
-    # sheet_load_Mirror_Server.write(row, col + 5, "本地上行(KB)")
-    # sheet_load_Mirror_Server.write(row, col + 6, "本地总流量(KB)")
-    #
-    # sheet_load_TXZ.write(row, col, "时间")
-    # sheet_load_TXZ.write(row, col + 1, "网络下行(KB)")
-    # sheet_load_TXZ.write(row, col + 2, "网络上行(KB)")
-    # sheet_load_TXZ.write(row, col + 3, "网络总流量(KB)")
-    # sheet_load_TXZ.write(row, col + 4, "本地下行(KB)")
-    # sheet_load_TXZ.write(row, col + 5, "本地上行(KB)")
-    # sheet_load_TXZ.write(row, col + 6, "本地总流量(KB)")
 
-    # ----------------- 需要监测的包名 -----------------
-    # package_name_Mirror = "cn.hollo.mirror"
-    # package_name_Mirror_Server = "cn.hollo.mirror.service"
-    # package_name_TXZ = "com.txznet.txz"
+    sheet_load_Mirror_Server.write(row, col, "时间")
+    sheet_load_Mirror_Server.write(row, col + 1, "网络下行(KB)")
+    sheet_load_Mirror_Server.write(row, col + 2, "网络上行(KB)")
+    sheet_load_Mirror_Server.write(row, col + 3, "网络总流量(KB)")
+    sheet_load_Mirror_Server.write(row, col + 4, "本地下行(KB)")
+    sheet_load_Mirror_Server.write(row, col + 5, "本地上行(KB)")
+    sheet_load_Mirror_Server.write(row, col + 6, "本地总流量(KB)")
+
+    sheet_load_TXZ.write(row, col, "时间")
+    sheet_load_TXZ.write(row, col + 1, "网络下行(KB)")
+    sheet_load_TXZ.write(row, col + 2, "网络上行(KB)")
+    sheet_load_TXZ.write(row, col + 3, "网络总流量(KB)")
+    sheet_load_TXZ.write(row, col + 4, "本地下行(KB)")
+    sheet_load_TXZ.write(row, col + 5, "本地上行(KB)")
+    sheet_load_TXZ.write(row, col + 6, "本地总流量(KB)")
+    #
+    # # ----------------- 需要监测的包名 -----------------
+    package_name_Mirror = "cn.hollo.mirror"
+    package_name_Mirror_Server = "cn.hollo.mirror"
+    package_name_TXZ = "cn.hollo.mirror"
     package_name_Mirror = packagename
-
-    try:
-        uid_Mirror = getUserId(package_name_Mirror)
-        print(time.strftime('%Y-%m-%d   %H:%M:%S', time.localtime(time.time())) + '  uid =  ' + str(uid_Mirror))
-    except:
-        print('获取Mirror-uid失败')
-
+    package_name_Mirror_Server = packagename
+    #
+    # try:
+    #     uid_Mirror = getUserId(package_name_Mirror)
+    #     print(time.strftime('%Y-%m-%d   %H:%M:%S', time.localtime(time.time())) + '  uid =  ' + str(uid_Mirror))
+    # except:
+    #     print('获取Mirror-uid失败')
+    #
     row = 1
     col = 0
 
@@ -202,7 +191,7 @@ if __name__ == '__main__':
             lo_flow_rx, lo_flow_tx = lo_end_rx - lo_start_rx, lo_end_tx - lo_start_tx
             net_rx_kb, net_tx_kb = round(net_flow_rx / 1024, 3), round(net_flow_tx / 1024, 3)
             lo_rx_kb, lo_tx_kb = round(lo_flow_rx / 1024, 3), round(lo_flow_tx / 1024, 3)
-            # rx_mb, tx_mb = round(flow_rx / 1024 / 1024, 3), round(flow_tx / 1024 / 1024, 3)
+
             timeNow = time.strftime('%Y-%m-%d   %H:%M:%S', time.localtime(time.time()))  # 获取当前时间
 
             sheet_load_Mirror.write(row, col, timeNow)  # 写入时间
@@ -223,25 +212,27 @@ if __name__ == '__main__':
                   '本地总流量', round(lo_rx_kb + lo_tx_kb, 3), 'KB\t'
                   )
 
-            # sheet_load_Mirror_Server.write(row, col, timeNow)  # 写入时间
-            # sheet_load_Mirror_Server.write(row, col + 1, net_rx_kb)  # 写入网络下行(KB)
-            # sheet_load_Mirror_Server.write(row, col + 2, net_tx_kb)  # 写入网络上行(KB)
-            # sheet_load_Mirror_Server.write(row, col + 3, round(net_rx_kb + net_tx_kb, 3))  # 写入网络总流量(KB)
-            # sheet_load_Mirror_Server.write(row, col + 4, lo_rx_kb)  # 写入本地上行(KB)
-            # sheet_load_Mirror_Server.write(row, col + 5, lo_tx_kb)  # 写入本地下行(KB)
-            # sheet_load_Mirror_Server.write(row, col + 6, round(lo_rx_kb + lo_tx_kb, 3))  # 写入本地总流量(KB)
-            #
-            # sheet_load_TXZ.write(row, col, timeNow)  # 写入时间
-            # sheet_load_TXZ.write(row, col + 1, net_rx_kb)  # 写入网络下行(KB)
-            # sheet_load_TXZ.write(row, col + 2, net_tx_kb)  # 写入网络上行(KB)
-            # sheet_load_TXZ.write(row, col + 3, round(net_rx_kb + net_tx_kb, 3))  # 写入网络总流量(KB)
-            # sheet_load_TXZ.write(row, col + 4, lo_rx_kb)  # 写入本地上行(KB)
-            # sheet_load_TXZ.write(row, col + 5, lo_tx_kb)  # 写入本地下行(KB)
-            # sheet_load_TXZ.write(row, col + 6, round(lo_rx_kb + lo_tx_kb, 3))  # 写入本地总流量(KB)
+            row = row + 1
 
+            sheet_load_Mirror_Server.write(row, col, timeNow)  # 写入时间
+            sheet_load_Mirror_Server.write(row, col + 1, net_rx_kb)  # 写入网络下行(KB)
+            sheet_load_Mirror_Server.write(row, col + 2, net_tx_kb)  # 写入网络上行(KB)
+            sheet_load_Mirror_Server.write(row, col + 3, round(net_rx_kb + net_tx_kb, 3))  # 写入网络总流量(KB)
+            sheet_load_Mirror_Server.write(row, col + 4, lo_rx_kb)  # 写入本地上行(KB)
+            sheet_load_Mirror_Server.write(row, col + 5, lo_tx_kb)  # 写入本地下行(KB)
+            sheet_load_Mirror_Server.write(row, col + 6, round(lo_rx_kb + lo_tx_kb, 3))  # 写入本地总流量(KB)
 
-            # book_Mirror_Server.save(r"d:\Mirror_ServerFlow.xls")
-            # book_TXZ.save(r"d:\TXZFolw.xls")
+            sheet_load_TXZ.write(row, col, timeNow)  # 写入时间
+            sheet_load_TXZ.write(row, col + 1, net_rx_kb)  # 写入网络下行(KB)
+            sheet_load_TXZ.write(row, col + 2, net_tx_kb)  # 写入网络上行(KB)
+            sheet_load_TXZ.write(row, col + 3, round(net_rx_kb + net_tx_kb, 3))  # 写入网络总流量(KB)
+            sheet_load_TXZ.write(row, col + 4, lo_rx_kb)  # 写入本地上行(KB)
+            sheet_load_TXZ.write(row, col + 5, lo_tx_kb)  # 写入本地下行(KB)
+            sheet_load_TXZ.write(row, col + 6, round(lo_rx_kb + lo_tx_kb, 3))  # 写入本地总流量(KB)
+
+            book_Mirror.save(r"d:\MirrorFlow.xls")
+            book_Mirror_Server.save(r"d:\Mirror_ServerFlow.xls")
+            book_TXZ.save(r"d:\TXZFolw.xls")
             if n == LIMIT:
                 break
         except KeyboardInterrupt:
